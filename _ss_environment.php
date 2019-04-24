ac56b78c144823fe023aa35a5450785107634be9
Niv Suresh <nivsuresh06@gmail.com>
2019-04-24 20:33:05 +0100

    old

--- _ss_environment.php
+++ _ss_environment.php
@@ -3,9 +3,11 @@
 //see:
 // /framework/conf/ConfigureFromEnv.php
 // http://doc.silverstripe.org/framework/en/topics/environment-management
-
+/*
 if(getenv('SERVER_DEVELOPMENT')){
 	require_once('_ss_environment.DEV.php');
 }else{
 	require_once('_ss_environment.LIVE.php');
-};
+};*/
+
+require_once('_ss_environment.DEV.php');
